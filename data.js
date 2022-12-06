0d3de303dcfd2b3e17367aa1e846598fa505cb3f
adamhayek2002 <116099346+GruneStrasse@users.noreply.github.com>
2022-12-06 12:57:26 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,192 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8833964634667808,
+          "pitch": -0.06300094076452645,
+          "rotation": 0,
+          "target": "4-01"
+        },
+        {
+          "yaw": 1.8755548948204783,
+          "pitch": -0.04776659745844469,
+          "rotation": 0,
+          "target": "1-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1498672968525696,
+          "pitch": 0.010978010887946255,
+          "rotation": 0,
+          "target": "3-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04",
+      "name": "04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9239798145620917,
+          "pitch": -0.006230842565969397,
+          "rotation": 0,
+          "target": "2-01"
+        },
+        {
+          "yaw": 1.42496791061029,
+          "pitch": 0.055420407838733254,
+          "rotation": 0,
+          "target": "4-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8991878465860594,
+          "pitch": -0.06322332709763501,
+          "rotation": 0,
+          "target": "3-04"
+        },
+        {
+          "yaw": 1.897127440862243,
+          "pitch": -0.02367927239196277,
+          "rotation": 0,
+          "target": "0-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
